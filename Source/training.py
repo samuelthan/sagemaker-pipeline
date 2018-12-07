c5e2a7402859d0d70d6a90f3fb408836e3bf982d
Agustinus Nalwan <agustinus.nalwan@carsales.com.au>
2018-12-07 18:08:47 +1100

    use-high-level-api

--- Source/training.py
+++ Source/training.py
@@ -61,151 +61,72 @@ train_size = 336
 # upload_to_s3('validation', 'caltech-256-60-val.rec')
 # print ("Finished Downloadng Testing Data")
 
-print ("Setting Algorithm Settings")
-print("Test!!!")
+s3_train_data = 's3://ai-sagemaker-objdetect/rego-plate-detection/train'
+s3_validation_data = 's3://ai-sagemaker-objdetect/rego-plate-detection/validation'
+s3_train_annotation = 's3://ai-sagemaker-objdetect/rego-plate-detection/train_annotation'
+s3_validation_annotation = 's3://ai-sagemaker-objdetect/rego-plate-detection/validation_annotation'
 
-# Specify the base network
-base_network = 'resnet-50'
-# For this training, we will use 18 layers
-label_width = 600
-# we need to specify the input image shape for the training data
-image_shape = 512
-# we also need to specify the number of training samples in the training set
-# for caltech it is 15420
-num_training_samples = train_size
-# specify the number of output classes
-num_classes = 1
-# batch size for training
-mini_batch_size = 16
-# number of epochs
-epochs = 1
-# learning rate
-learning_rate = 0.001
+s3_output_location = 's3://ai-sagemaker-objdetect/rego-plate-detection/output'
+
+import sagemaker
+from sagemaker.amazon.amazon_estimator import get_image_uri
+
+sess = sagemaker.Session()
+training_image = get_image_uri(sess.boto_region_name, 'object-detection', repo_version="latest")
+print (training_image)
+
+
+od_model = sagemaker.estimator.Estimator(training_image,
+                                         role,
+                                         train_instance_count=1,
+                                         train_instance_type='ml.p2.16xlarge',
+                                         train_volume_size = 50,
+                                         train_max_run = 360000,
+                                         input_mode = 'File',
+                                         output_path=s3_output_location,
+                                         sagemaker_session=sess)
+
+od_model.set_hyperparameters(base_network='resnet-50',
+                             use_pretrained_model=1,
+                             num_classes=1,
+                             mini_batch_size=16,
+                             epochs=30,
+                             learning_rate=0.001,
+                             lr_scheduler_step='10',
+                             lr_scheduler_factor=0.1,
+                             optimizer='sgd',
+                             momentum=0.9,
+                             weight_decay=0.0005,
+                             overlap_threshold=0.5,
+                             nms_threshold=0.45,
+                             image_shape=512,
+                             label_width=600,
+                             num_training_samples=train_size)
+
+train_data = sagemaker.session.s3_input(s3_train_data, distribution='FullyReplicated',
+                        content_type='image/jpeg', s3_data_type='S3Prefix')
+validation_data = sagemaker.session.s3_input(s3_validation_data, distribution='FullyReplicated',
+                             content_type='image/jpeg', s3_data_type='S3Prefix')
+train_annotation = sagemaker.session.s3_input(s3_train_annotation, distribution='FullyReplicated',
+                             content_type='image/jpeg', s3_data_type='S3Prefix')
+validation_annotation = sagemaker.session.s3_input(s3_validation_annotation, distribution='FullyReplicated',
+                             content_type='image/jpeg', s3_data_type='S3Prefix')
+
+data_channels = {'train': train_data, 'validation': validation_data,
+                 'train_annotation': train_annotation, 'validation_annotation':validation_annotation}
+
+od_model.fit(inputs=data_channels, logs=True)
+
 
 # create unique job name
 job_name = stack_name + "-" + commit_id + "-" + timestamp
-training_params = \
-{
-    # specify the training docker image
-    "AlgorithmSpecification": {
-        "TrainingImage": training_image,
-        "TrainingInputMode": "File"
-    },
-    "RoleArn": role,
-    "OutputDataConfig": {
-        "S3OutputPath": 's3://{}'.format(bucket)
-    },
-    "ResourceConfig": {
-        "InstanceCount": 1,
-        "InstanceType": "ml.p2.8xlarge",
-        "VolumeSizeInGB": 50
-    },
-    "TrainingJobName": job_name,
-    "HyperParameters": {
-        "base_network": base_network,
-        "use_pretrained_model": "1",
-        "num_classes": str(num_classes),
-        "mini_batch_size": str(mini_batch_size),
-        "epochs": str(epochs),
-        "learning_rate": str(learning_rate),
-        "lr_scheduler_step": str(10),
-        "lr_scheduler_factor": str(0.1),
-        "optimizer": "sgd",
-        "momentum": str(0.9),
-        "weight_decay": str(0.0005),
-        "overlap_threshold": str(0.5),
-        "nms_threshold": str(0.45),
-        "image_shape": str(image_shape),
-        "label_width": str(label_width),
-        "num_training_samples": str(train_size)
-    },
-    "StoppingCondition": {
-        "MaxRuntimeInSeconds": 360000
-    },
-#Training data should be inside a subdirectory called "train" and "train_annotation"
-#Validation data should be inside a subdirectory called "validation" and "validation_annotation"
-#The algorithm currently only supports fullyreplicated model (where data is copied onto each machine)
-    "InputDataConfig": [
-        {
-            "ChannelName": "train",
-            "DataSource": {
-                "S3DataSource": {
-                    "S3DataType": "S3Prefix",
-                    "S3Uri": 's3://ai-sagemaker-objdetect/rego-plate-detection/train/'.format(bucket, prefix),
-                    "S3DataDistributionType": "FullyReplicated"
-                }
-            },
-            "ContentType": "image/jpeg",
-            "CompressionType": "None"
-        },
-        {
-            "ChannelName": "validation",
-            "DataSource": {
-                "S3DataSource": {
-                    "S3DataType": "S3Prefix",
-                    "S3Uri": 's3://ai-sagemaker-objdetect/rego-plate-detection/validation/'.format(bucket, prefix),
-                    "S3DataDistributionType": "FullyReplicated"
-                }
-            },
-            "ContentType": "image/jpeg",
-            "CompressionType": "None"
-        },
-        {
-            "ChannelName": "train_annotation",
-            "DataSource": {
-                "S3DataSource": {
-                    "S3DataType": "S3Prefix",
-                    "S3Uri": 's3://ai-sagemaker-objdetect/rego-plate-detection/train_annotation/'.format(bucket, prefix),
-                    "S3DataDistributionType": "FullyReplicated"
-                }
-            },
-            "ContentType": "image/jpeg",
-            "CompressionType": "None"
-        },
-        {
-            "ChannelName": "validation_annotation",
-            "DataSource": {
-                "S3DataSource": {
-                    "S3DataType": "S3Prefix",
-                    "S3Uri": 's3://ai-sagemaker-objdetect/rego-plate-detection/validation_annotation/'.format(bucket, prefix),
-                    "S3DataDistributionType": "FullyReplicated"
-                }
-            },
-            "ContentType": "image/jpeg",
-            "CompressionType": "None"
-        }
-    ]
-}
-print('Training job name: {}'.format(job_name))
-print('\nInput Data Location: {}'.format(training_params['InputDataConfig'][0]['DataSource']['S3DataSource']))
-
-# create the Amazon SageMaker training job
-sagemaker = boto3.client(service_name='sagemaker')
-sagemaker.create_training_job(**training_params)
-
-# confirm that the training job has started
-status = sagemaker.describe_training_job(TrainingJobName=job_name)['TrainingJobStatus']
-print('Training job current status: {}'.format(status))
-
-try:
-    # wait for the job to finish and report the ending status
-    sagemaker.get_waiter('training_job_completed_or_stopped').wait(TrainingJobName=job_name)
-    training_info = sagemaker.describe_training_job(TrainingJobName=job_name)
-    status = training_info['TrainingJobStatus']
-    print("Training job ended with status: " + status)
-except:
-    print('Training failed to start')
-     # if exception is raised, that means it has failed
-    message = sagemaker.describe_training_job(TrainingJobName=job_name)['FailureReason']
-    print('Training failed with the following error: {}'.format(message))
-
 
 # creating configuration files so we can pass parameters to our sagemaker endpoint cloudformation
 
 config_data_qa = {
   "Parameters":
     {
-        "BucketName": bucket,
+        "BucketName": s3_output_location,
         "CommitID": commit_id,
         "Environment": "qa",
         "ParentStackName": stack_name,
@@ -219,7 +140,7 @@ config_data_qa = {
 config_data_prod = {
   "Parameters":
     {
-        "BucketName": bucket,
+        "BucketName": s3_output_location,
         "CommitID": commit_id,
         "Environment": "prod",
         "ParentStackName": stack_name,
